52f8ac3b9a3ba5ecf1a8f98e6ac38bc742bcc934
LordChusmimax <jmpintosaez@gmail.com>
2020-07-29 13:14:44 +0200

    saltos y sus scripts

---
+++ Assets/GroundCheckScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckScript : MonoBehaviour
+{
+    public bool isGrounded = false;
+    private Rigidbody2D rb;
+    private GameObject player;
+
+    private void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+        player = transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -11,10 +11,22 @@ public class PlayerScript : MonoBehaviour
     protected Rigidbody2D rb;
     protected Collider2D col;
     protected bool faceRight = false;
+    protected bool jumpPressed;
+    protected int forceFall;
+    [SerializeField] protected float forcedFallTimer = 0.3f;
+    protected bool grounded;
+    [SerializeField] protected float jumpForce = 10f;
+    [SerializeField] protected float maxYVelocity = -10;
+    protected GroundCheckScript gc;
+    protected float highGravity;
+
+    [SerializeField] protected float highGravityMod = 1.5f;
 
 
     private void Awake()
     {
+        highGravity = Physics2D.gravity.y * highGravityMod;
+        gc = GetComponentInChildren<GroundCheckScript>();
         controls = new PlayerInputs();
         controls.Player.Enable();
         col = GetComponent<Collider2D>();
@@ -33,10 +45,16 @@ public class PlayerScript : MonoBehaviour
         Move();
     }
 
+    protected void FixedUpdate()
+    {
+        grounded = gc.isGrounded;
+        Fall();
+    }
+
     //assigns methods to every action in the mapping
     protected void ActionAssigner()
     {
-        //controls.Player.Jump.performed += ctx => Jump();
+        controls.Player.Jump.performed += ctx => Jump();
         //controls.Player.Jump.canceled += ctx => JumpEnd();
         //controls.Player.Shoot.performed += ctx => Shoot();
         //controls.Player.Pick.performed += ctx => Pick();
@@ -75,4 +93,39 @@ public class PlayerScript : MonoBehaviour
             transform.localScale = new Vector2(-Mathf.Sign(movement), 1) * currentScale;
         }
     }
+
+    //controlls the high gravity to reduce the feel of floatiness on jumps and falls
+    public void Fall()
+    {
+        if ((rb.velocity.y < 0 || forceFall == 1) && rb.velocity.y > maxYVelocity)
+        {
+            rb.velocity += Vector2.up * (highGravity) * Time.fixedDeltaTime;
+        }
+    }
+
+    //controlls everything related to jumps
+    public void Jump()
+    {
+        jumpPressed = true;
+        if (grounded)
+        {
+            forceFall = 0;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            StartCoroutine(NotThatHigh());
+        }
+    }
+
+    public void JumpEnd()
+    {
+        jumpPressed = false;
+        forceFall = 1;
+    }
+
+    //forces the high gravity after X time even if you keep pressing "jump"
+    protected IEnumerator NotThatHigh()
+    {
+        yield return new WaitForSeconds(forcedFallTimer);
+        forceFall = 1;
+    }
+
 }
