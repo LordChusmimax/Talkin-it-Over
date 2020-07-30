da085e738641206084385825f8c47e451ecc51cd
Alejandro Gómez Lucena <56355572+agomezlucena@users.noreply.github.com>
2020-07-30 19:35:24 +0200

    Create JoypadAimAimAnimator.cs

---
+++ Assets/Script/Player/JoypadAimAimAnimator.cs
@@ -0,0 +1,16 @@
+ public class JoypadAimAimAnimator:AimAnimator{
+        overrides void aim() {
+             var aimVec = controls.Player.Aim.ReadValue<Vector2>();
+                if (aimVec.magnitude > deadZone)
+                {
+                    var aimAngle = math.atan2(aimVec.y, aimVec.x) * 180 / math.PI;
+                    aimAngle = faceLeft ? aimAngle : aimAngle + 180;
+                    rightArm.eulerAngles = new Vector3(rightArm.rotation.eulerAngles.x, rightArm.rotation.eulerAngles.y, aimAngle);
+                }
+                else
+                {
+                    var aimAngle = faceLeft ? -130 : 130;
+                    rightArm.eulerAngles = new Vector3(rightArm.rotation.eulerAngles.x, rightArm.rotation.eulerAngles.y, aimAngle);
+                }
+        }
+    }
