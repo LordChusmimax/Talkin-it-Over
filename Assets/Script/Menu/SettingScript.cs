0a6b8eaebd3a144aa1e585ffd61d1312163ba2a3
José Luis Gómez Miño <joseluisgomezmino@hotmail.com>
2020-07-31 13:56:24 +0200

    Menu - Modificación del menu completo

---
+++ Assets/Script/Menu/SettingScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingScript : MonoBehaviour
+{
+    
+
+    public void setPantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+}
