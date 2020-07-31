ec35c0e8441307531b8de25ce66501852380c206
LordChusmimax <jmpintosaez@gmail.com>
2020-07-31 15:16:54 +0200

    Cinemachines, Son
    Añadido Cinemachine al Lab

--- Assets/Script/Player/MouseAimAnimator.cs
+++ Assets/Script/Player/MouseAimAnimator.cs
@@ -16,7 +16,7 @@ public class MouseAimAnimator:AimAnimator{
         void AimAnimator.Aim(bool faceLeft) {
             var aimVec = controls.Player.MousePosition.ReadValue<Vector2>();
             aimVec = camera.ScreenToWorldPoint(aimVec);
-            aimVec = aimVec - (Vector2)rightArm.position;
+            aimVec = (Vector3)aimVec - rightArm.position;
             var aimAngle = math.atan2(aimVec.y, aimVec.x) * 180 / math.PI;
             aimAngle = faceLeft ? aimAngle : aimAngle + 180;
             rightArm.eulerAngles = new Vector3(rightArm.rotation.eulerAngles.x, rightArm.rotation.eulerAngles.y, aimAngle);
