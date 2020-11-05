f3372f28400ce6f3ac9a604635ed74851b8fed4f
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2020-11-05 18:58:55 +0100

    sountrack implemented

---
+++ Assets/Script/Sound/Music/ManageSound.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageSound : MonoBehaviour
+{
+
+    private AudioSource song;
+    public AudioClip[] songList;
+    private bool songStarted = false;
+    private static ManageSound current;
+    [SerializeField] private float musicVolume = 0.25f;
+
+    void Start()
+
+    {
+        if (current != null)
+        {
+            GameObject.Destroy(this);
+        }
+        else
+        {
+            current = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        song = GetComponent<AudioSource>();
+        StartSong();
+    }
+
+    public void EndSong()
+    {
+        song.Stop();
+    }
+
+    public void StartSong()
+    {
+        song.Play();
+        song.volume = musicVolume;
+    }
+
+    public void PauseSong()
+    {
+        song.Pause();
+    }
+
+    public void ResumeSong()
+    {
+        song.UnPause();
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Lab")
+        {
+            EndSong();
+            song.clip = songList[1];
+            StartSong();
+        }
+    }
+}
