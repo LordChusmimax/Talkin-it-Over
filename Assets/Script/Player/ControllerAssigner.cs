c11fb65cad5a013ba2f2425f5a1cce7e0ebd36bd
josep93 <joseluisgomezmino@hotmail.com>
2021-01-13 13:42:42 +0100

    Menu - Implementado sistema de Skins

--- Assets/Script/Player/ControllerAssigner.cs
+++ Assets/Script/Player/ControllerAssigner.cs
@@ -19,6 +19,8 @@ public class ControllerAssigner : MonoBehaviour
         {
             PlayerScript player = Instantiate(prefabPlayer, transform).GetComponent<PlayerScript>();
             player.SelectController(elemento.Key);
+
+            Debug.Log("Mi skin es: " + elemento.Value);
         }
     }
 
