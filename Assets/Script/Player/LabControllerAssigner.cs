5b1474ab95d4ce0d1ba70f4daba641bb34fc140e
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-13 20:11:37 +0100

    scene 1 complete

--- Assets/Script/Player/LabControllerAssigner.cs
+++ Assets/Script/Player/LabControllerAssigner.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LabControllerAssigner : ControllerAssigner
 {
+    private Transform[] spawners;
+    private GameObject spawnerCollector;
+
     void Start()
     {
         current = this;
@@ -13,6 +18,30 @@ public class LabControllerAssigner : ControllerAssigner
             player.SelectController(index);
             index++;
         }
+
+        spawnerCollector = GetComponentInChildren<SpawnerScript>().gameObject;
+        spawners = spawnerCollector.GetComponentsInChildren<Transform>();
+
+        List<int> randomOrder = randomNumberList();
+
+        int i = 0;
+        foreach (PlayerScript player in players)
+        {
+            player.transform.position=spawners[randomOrder[i]].transform.position;
+            i++;
+        }
+
+    }
+
+    private List<int> randomNumberList()
+    {
+        List<int> randomOrder = new List<int>();
+        randomOrder.Add(1);
+        randomOrder.Add(2);
+        randomOrder.Add(3);
+        randomOrder.Add(4);
+        randomOrder = randomOrder.OrderBy(j => Guid.NewGuid()).ToList();
+        return randomOrder;
     }
 
 }
