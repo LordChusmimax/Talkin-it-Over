edaa2a4752bd92c8244e81c46ce8201c5ceebec0
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-02-11 19:46:13 +0100

    first bot updated

--- Assets/Script/Player/Aim/MouseAimAnimator.cs
+++ Assets/Script/Player/Aim/MouseAimAnimator.cs
@@ -1,7 +1,8 @@
 using Unity.Mathematics;
 using UnityEngine;
 
-public class MouseAimAnimator:AimAnimator{
+public class MouseAimAnimator : AimAnimator
+{
     PlayerInputs controls;
     private Transform rightArm;
     private Camera camera;
@@ -13,12 +14,13 @@ public class MouseAimAnimator:AimAnimator{
         this.camera = camera;
     }
 
-        void AimAnimator.Aim(bool faceLeft) {
-            var aimVec = controls.Player.MousePosition.ReadValue<Vector2>();
-            aimVec = camera.ScreenToWorldPoint(aimVec);
-            aimVec = (Vector3)aimVec - rightArm.position;
-            var aimAngle = math.atan2(aimVec.y, aimVec.x) * 180 / math.PI;
-            aimAngle = faceLeft ? aimAngle : aimAngle + 180;
-            rightArm.eulerAngles = new Vector3(rightArm.rotation.eulerAngles.x, rightArm.rotation.eulerAngles.y, aimAngle);
-        }
+    void AimAnimator.Aim(bool faceLeft)
+    {
+        var aimVec = controls.Player.MousePosition.ReadValue<Vector2>();
+        aimVec = camera.ScreenToWorldPoint(aimVec);
+        aimVec = (Vector3)aimVec - rightArm.position;
+        var aimAngle = math.atan2(aimVec.y, aimVec.x) * 180 / math.PI;
+        aimAngle = faceLeft ? aimAngle : aimAngle + 180;
+        rightArm.eulerAngles = new Vector3(rightArm.rotation.eulerAngles.x, rightArm.rotation.eulerAngles.y, aimAngle);
     }
+}
