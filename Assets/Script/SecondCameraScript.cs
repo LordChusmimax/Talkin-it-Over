426ac05a53c46967216c8a8a817783cdcbd69f32
Jesús Miguel Pinto Sáez <jmpintosaez@gmail.com>
2021-09-01 02:54:09 +0200

    nueva metropolis

---
+++ Assets/Script/SecondCameraScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondCameraScript : MonoBehaviour
+{
+
+    Camera mainCamera,camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = transform.parent.GetComponent<Camera>();
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.orthographicSize = mainCamera.orthographicSize*3f;
+    }
+}
