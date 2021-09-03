e31cba8f34f92e108587b77c02e8d90bce1e622c
Jesús Miguel Pinto Sáez <jmpintosaez@gmail.com>
2021-09-04 00:36:11 +0200

    update linkVariable

--- Assets/Script/LinkScript.cs
+++ Assets/Script/LinkScript.cs
@@ -5,7 +5,7 @@ using Unity.Mathematics;
 
 public class LinkScript : MonoBehaviour
 {
-    public GameObject BeamV;
+    public GameObject Targer;
     public SpriteRenderer spriteRenderer;
     void Start()
     {
@@ -15,11 +15,11 @@ public class LinkScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var dirVec = (Vector3)BeamV.transform.position - transform.position;
+        var dirVec = (Vector3)Targer.transform.position - transform.position;
         var dirAngle = math.atan2(dirVec.y, dirVec.x) * 180 / math.PI;
         transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, dirAngle-90);
 
-        var distance = Vector2.Distance(BeamV.transform.position, transform.position)*20;
+        var distance = Vector2.Distance(Targer.transform.position, transform.position)*20;
         spriteRenderer.size = new Vector2(spriteRenderer.size.x, distance);
     }
 }
